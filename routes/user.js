0e7bafccf08039fa464a4b8ce357ff543eec44ba
suportdevs <suportdevs@gmail.com>
2023-12-22 09:22:21 +0600

    working on income components

--- routes/user.js
+++ routes/user.js
@@ -68,24 +68,27 @@ router.get('/', verifyToken, async (req, res) => {
 // user stats
 router.get('/stats', verifyToken, async (req, res) => {
     const date = new Date();
-    const lastYear = new Date(date.getFullYear(date.getFullYear() - 1));
-    try{
-        const data = await User.aggregate([
-            {$match :{
-                createdAt: {$gte: lastYear}
-            }},
-            {$project: {
-                month: {$month: "$createdAt"}
-            }},
-            {$group: {
-                _id: "$month",
-                total: {$sum: 1},
-            }}
-        ]);
-        res.status(200).json(data);
-    }catch(err){
-        res.status(500).json(err);
-    }
+  const lastYear = new Date(date.setFullYear(date.getFullYear() - 1));
+
+  try {
+    const data = await User.aggregate([
+      { $match: { createdAt: { $gte: lastYear } } },
+      {
+        $project: {
+          month: { $month: "$createdAt" },
+        },
+      },
+      {
+        $group: {
+          _id: "$month",
+          total: { $sum: 1 },
+        },
+      },
+    ]);
+    res.status(200).json(data)
+  } catch (err) {
+    res.status(500).json(err);
+  }
 })
 
 module.exports = router;
